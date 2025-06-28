1ff2ec1060c92f92cba82b001a5c5cd0f76317c6
Maximus Sallam <maximus.sallam@gmail.com>
2025-06-27 23:18:55 -0400

    work is life

--- MaximusHttpClientTrait.php
+++ MaximusHttpClientTrait.php
@@ -4,13 +4,19 @@ namespace MaximusSallam\SymfonyOverrides;
 
 use Symfony\Component\HttpClient\HttpClientTrait as SymfonyHttpClientTrait;
 
-class MaximusHttpClientTrait extends SymfonyHttpClientTrait
+class MaximusHttpClientTrait
 {
+    use SymfonyHttpClientTrait;  // Use the original HttpClientTrait
+
+    /**
+     * Overriding the proxy URL behavior to strip 'ssl://' prefix.
+     */
     protected function createProxyUrl(array $proxy): string
     {
-        $proxyUrl = parent::createProxyUrl($proxy);
+        $proxyUrl = parent::createProxyUrl($proxy);  // Call the original method from HttpClientTrait
 
         // Remove 'ssl://' from proxy URL if present
         return str_replace('ssl://', '', $proxyUrl);  // Strip 'ssl://'
     }
 }
+
