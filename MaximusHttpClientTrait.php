e89a854a36f37ba642bb634ad4f82d17e575e2a4
Maximus Sallam <maximus.sallam@gmail.com>
2025-06-27 23:14:29 -0400

    paving the way

--- MaximusHttpClientTrait.php
+++ MaximusHttpClientTrait.php
@@ -1,11 +1,16 @@
 <?php
+
 namespace MaximusSallam\SymfonyOverrides;
+
 use Symfony\Component\HttpClient\HttpClientTrait as SymfonyHttpClientTrait;
-class MaximusHttpClientTrait extends SymfonyHttpClientTrait {
-    // Override the proxy URL behavior to remove 'ssl://'
-    protected function createProxyUrl(array $proxy): string {
+
+class MaximusHttpClientTrait extends SymfonyHttpClientTrait
+{
+    protected function createProxyUrl(array $proxy): string
+    {
         $proxyUrl = parent::createProxyUrl($proxy);
-        return str_replace('ssl://', '', $proxyUrl); // Remove 'ssl://' from proxy URL
+
+        // Remove 'ssl://' from proxy URL if present
+        return str_replace('ssl://', '', $proxyUrl);  // Strip 'ssl://'
     }
 }
-
--- MaximusSocketStream.php
+++ MaximusSocketStream.php
@@ -1,11 +1,16 @@
 <?php
 namespace MaximusSallam\SymfonyOverrides;
+
 use Symfony\Component\Mailer\Transport\Smtp\Stream\SocketStream as SymfonySocketStream;
-class MaximusSocketStream extends SymfonySocketStream {
-    public function __construct(string $url, float $timeout = null, float $idleTimeout = null) {
-        // Prevent 'ssl://' from being prepended
+
+class MaximusSocketStream extends SymfonySocketStream
+{
+    public function __construct(string $url, float $timeout = null, float $idleTimeout = null)
+    {
+        // Remove 'ssl://' from the URL if it exists
         $url = str_replace('ssl://', '', $url);
+        
+        // Call the parent constructor with the modified URL
         parent::__construct($url, $timeout, $idleTimeout);
     }
 }
-
